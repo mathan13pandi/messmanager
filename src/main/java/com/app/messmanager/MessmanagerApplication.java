c42a8cf19ee68cdc079d613806e653e63ee4be53
matha <mathan13siva@gmail.com>
2022-05-22 18:43:14 +0530

    Added spring project to git.

---
+++ src/main/java/com/app/messmanager/MessmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.app.messmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/messmanager/MessmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.messmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
