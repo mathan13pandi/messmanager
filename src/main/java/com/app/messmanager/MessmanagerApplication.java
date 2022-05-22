2c482d64721f3ea59d3f64f6b399b762fff1cdd5
matha <mathan13siva@gmail.com>
2022-05-22 19:12:11 +0530

    Comments added

--- src/main/java/com/app/messmanager/MessmanagerApplication.java
+++ src/main/java/com/app/messmanager/MessmanagerApplication.java
@@ -3,9 +3,16 @@ package com.app.messmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Class for MessManager Application.
+ */
 @SpringBootApplication
 public class MessmanagerApplication {
 
+	/**
+	 * Main method for launching the app.
+	 * @param args command line args.
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(MessmanagerApplication.class, args);
 	}
